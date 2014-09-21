ddaea2bae59543b11f9be76d97d3fca545095ffc
Arnaud Huc <arnaud-huc@cifacom.com>
2014-09-21 13:11:30 +0200

    fix bug ip

--- app.js
+++ app.js
@@ -46,15 +46,18 @@ if(result == ''){
 }
 console.log('ip ' + result);
 
+ip = result.split(' ');
+
+console.log('"'+ip[0]+'"');
 // Mongo
 var db = mongo.MongoClient;
 
 /*
  * Routes
  */
-app.get('/',index.index(result, db));
-app.get('/new', index.newGame(result, db));
-app.post('/new', index.newGame(result, db));
+app.get('/',index.index(ip[0], db));
+app.get('/new', index.newGame(ip[0], db));
+app.post('/new', index.newGame(ip[0], db));
 
 var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,18 @@
  */
 // Set the connexion to your mongo database.
 var dbconnection = "mongodb://127.0.0.1:27017/donjongradon";
-exports.index = function(result, db){
+exports.index = function(ip, db){
     return function(req, res){
         // connect into your mongo database
         db.connect(dbconnection, function(err, db) {
             if(err) throw err;
             // Choose your table
-            res.render('home/index', { title: 'Donjon & Gradon', ip: result });
+            res.render('home/index', { title: 'Donjon & Gradon', ip: ip });
         });
     }
 };
 
-exports.newGame = function(result, db){
+exports.newGame = function(ip, db){
     return function(req, res){
         console.log(req.body.test);
         if(req.body.test) {
@@ -26,7 +26,7 @@ exports.newGame = function(result, db){
             socket.hero.test = req.body.test;
             console.log(socket.hero);
         } else {
-            res.render('home/new', {title : 'Donjon & Gradon - New', ip : result});
+            res.render('home/new', {title : 'Donjon & Gradon - New', ip : ip});
         }
     }
 };
