011135d5945fc96a6b5d0159df364ef18c514668
Arnaud Huc <arnaud-huc@cifacom.com>
2014-10-19 16:42:28 +0200

    Refresh installation for canvas

---
+++ public/javascripts/canvas/map.js
@@ -0,0 +1,27 @@
+function Map(name){
+  // Création de l'objet XmlHttpRequest
+  var xhr = getXMLHttpRequest();
+
+  // Chargement du fichier
+  xhr.open("GET", '/maps/' + name + '.json', false);
+  xhr.send(null);
+  if(xhr.readyState != 4 || (xhr.status != 200 && xhr.status != 0)){ // Code == 0 en local
+    throw new Error("Impossible de charger la carte nommée \"" + name + "\" (code HTTP : " + xhr.status + ").");
+  }
+  var mapJsonData = xhr.responseText;
+
+  // Analyse des données
+  var mapData = JSON.parse(mapJsonData);
+
+  // Init tileset & map
+  this.tileset = new Tileset(mapData.tileset);
+  this.map = mapData.map;
+}
+
+// Pour récupérer la taille (en tiles) de la carte
+Map.prototype.getHeight = function() {
+  return this.terrain.length;
+}
+Map.prototype.getWidth = function() {
+  return this.terrain[0].length;
+}
