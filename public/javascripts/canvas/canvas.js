1ca96b7732711babbbbfa98e05b961f05d662a60
Arnaud Huc <arnaud@MacBook-Pro-de-Arnaud.local>
2014-12-02 10:34:56 +0100

    begin character

--- public/javascripts/canvas/canvas.js
+++ public/javascripts/canvas/canvas.js
@@ -1,64 +1,40 @@
-var smiley = new Image(),
-    ts = new Tileset("tile.png"),
-    map = new Map("map");
-
-smiley.src = "http://www.siteduzero.com/Templates/images/smilies/heureux.png";
+var hero = new Image();
+hero.src = "/images/character.png";
 
 window.onload = function () {
     var canvas = document.getElementById('screen');
     if (canvas) {
         var ctx = canvas.getContext('2d'),
-            image = {
+            positionImage = {
                 x: 10,
                 y: 10
             };
-
-        /* Draw Full rectangle */
-        //ctx.fillStyle = 'blue';
-        //ctx.fillRect(10, Math.LN100, 100, 50);
-
-        /* Draw Border of rectangle */
-        //ctx.strokeStyle = 'red';
-        //ctx.strokeRect(75, 75, 50, 50);
-
-        /* Draw Image */
-
-        /* Draw tileset */
-        //ts.drawTile(1, ctx, 10, 10);
-        //ts.drawTile(3, ctx, 42, 10);
-        //ts.drawTile(6, ctx, 90, 10);
-        //ts.drawTile(7, ctx, 130, 10);
-
-        /* Draw Json Map */
-        // canvas.width  = map.getWidth() * 32;
-        // canvas.height = map.getHeight() * 32;
-
-
-        ctx.drawImage(smiley, image.x, image.y);
+        
+        ctx.drawImage(hero, 0, 0, 16, 16, positionImage.x, positionImage.y, 16, 16);
 
         //map.drawMap(ctx);
 
         window.addEventListener("keyup", function (e) {
             switch (e.keyCode) {
-            case 38:
+            case 38: // top
                 clearScreen(ctx);
-                image.y = image.y - 3;
-                ctx.drawImage(smiley, image.x, image.y);
+                positionImage.y = positionImage.y - 3;
+                ctx.drawImage(hero, 16, 0, 16, 16, positionImage.x, positionImage.y, 16, 16);
                 break;
-            case 40:
+            case 40: // bottom
                 clearScreen(ctx);
-                image.y = image.y + 3;
-                ctx.drawImage(smiley, image.x, image.y);
+                positionImage.y = positionImage.y + 3;
+                ctx.drawImage(hero, 0, 0, 16, 16, positionImage.x, positionImage.y, 16, 16);
                 break;
-            case 37:
+            case 37: // left
                 clearScreen(ctx);
-                image.x = image.x - 3;
-                ctx.drawImage(smiley, image.x, image.y);
+                positionImage.x = positionImage.x - 3;
+                ctx.drawImage(hero, 32, 0, 16, 16, positionImage.x, positionImage.y, 16, 16);
                 break;
-            case 39:
+            case 39: //right
                 clearScreen(ctx);
-                image.x = image.x + 3;
-                ctx.drawImage(smiley, image.x, image.y);
+                ctx.drawImage(hero, 96, 0, 16, 16, positionImage.x, positionImage.y, 16, 16);
+                positionImage.x = positionImage.x + 3;
                 break;
             }
         }, false);
