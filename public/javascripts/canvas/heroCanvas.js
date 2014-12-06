067a701d86044f40c845252d6bb225209ce4257d
Arnaud Huc <arnaud@MacBook-Pro-de-Arnaud.local>
2014-12-06 23:44:00 +0100

    add requestAnimationFrame

--- public/javascripts/canvas/heroCanvas.js
+++ public/javascripts/canvas/heroCanvas.js
@@ -3,43 +3,87 @@ var heroCanvas = {
         this.drawHero();
     },
     drawHero : function(){
-        var hero = new Image();
-        hero.src = "/images/character.png";
-        var canvas = document.getElementById('heroCanvas');
-        if (canvas) {
-            canvas.width = 160;
-            canvas.height = 144;
-            var ctx = canvas.getContext('2d'),
-                positionImage = {
-                    x: canvas.width / 2,
-                    y: canvas.height / 2
-                };
-            ctx.drawImage(hero, 0, 0, 16, 16, positionImage.x, positionImage.y, 16, 16);
-            window.addEventListener("keydown", function (e) {
-                switch (e.keyCode) {
-                case 38: // top
-                    ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
-                    positionImage.y = positionImage.y - 3;
-                    ctx.drawImage(hero, 0, 16, 16, 16, positionImage.x, positionImage.y, 16, 16);
+        var hero = new Image(),
+            canvas = document.getElementById('heroCanvas'),
+            ctx = canvas.getContext('2d'),
+            requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+            window.requestAnimationFrame = requestAnimationFrame;
+        window.addEventListener("keydown", onKeyDown, false);
+        window.addEventListener("keyup", onKeyUp, false);
+        function onKeyDown(event) {
+            var keyCode = event.keyCode;
+            console.log(keyCode);
+            switch (keyCode) {
+                case 37: //Left
+                    keyLeft = true;
                     break;
-                case 40: // bottom
-                    ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
-                    positionImage.y = positionImage.y + 3;
-                    ctx.drawImage(hero, 0, 0, 16, 16, positionImage.x, positionImage.y, 16, 16);
+                case 38: //Top
+                    keyTop = true;
                     break;
-                case 37: // left
-                    ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
-                    positionImage.x = positionImage.x - 3;
-                    ctx.drawImage(hero, 0, 48, 16, 16, positionImage.x, positionImage.y, 16, 16);
+                case 39: //Right
+                    keyRight = true;
                     break;
-                case 39: //right
-                    ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
-                    ctx.drawImage(hero, 0, 32, 16, 16, positionImage.x, positionImage.y, 16, 16);
-                    positionImage.x = positionImage.x + 3;
+                case 40: //Bottom
+                    keyBottom = true;
                     break;
-                }
-            }, false);
+            }
         }
+
+        function onKeyUp(event) {
+            var keyCode = event.keyCode;
+            switch (keyCode) {
+                case 37: //Left
+                    keyLeft = false;
+                    break;
+                case 38: //Top
+                    keyTop = false;
+                    break;
+                case 39: //Right
+                    keyRight = false;
+                    break;
+                case 40: //Bottom
+                    keyBottom = false;
+                    break;
+            }
+        }
+
+        //neccessary variables
+        var tickX = 10,
+            tickY = 10,
+            keyLeft = false,
+            keyTop = false,
+            keyRight = false,
+            keyBottom = false;
+        //main animation function
+        function drawStuff() {
+            window.requestAnimationFrame(drawStuff);
+            hero.src = "/images/character.png";
+            hero.onload = function(){
+                console.log("image loaded");
+                   ctx.drawImage(hero, 0, 0, 16, 16, tickX, tickY, 16, 16);
+            };
+            if (keyRight == true) {
+                tickX += 1;
+                ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
+                ctx.drawImage(hero, 0, 32, 16, 16, tickX, tickY, 16, 16);
+            }
+            if (keyBottom == true) {
+                tickY += 1;
+                ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
+                ctx.drawImage(hero, 0, 0, 16, 16, tickX, tickY, 16, 16);
+            }
+            if (keyTop == true) {
+                tickY--;
+                ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
+                ctx.drawImage(hero, 0, 16, 16, 16, tickX, tickY, 16, 16);
+            }
+            if (keyLeft == true) {
+                tickX--;
+                ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
+                ctx.drawImage(hero, 0, 48, 16, 16, tickX, tickY, 16, 16);
+            }
+        }
+        window.requestAnimationFrame(drawStuff);
     }
 }
 
