a550340c6ec3d32a15fecad5c6b80bb96c6c17c6
Arnaud Huc <arnaud@MacBook-Pro-de-Arnaud.local>
2014-12-07 00:32:22 +0100

    Add setInterval()

--- public/javascripts/canvas/heroCanvas.js
+++ public/javascripts/canvas/heroCanvas.js
@@ -46,43 +46,54 @@ var heroCanvas = {
                     break;
             }
         }
-
         //neccessary variables
         var tickX = 10,
             tickY = 10,
             keyLeft = false,
             keyTop = false,
             keyRight = false,
-            keyBottom = false;
+            keyBottom = false,
+            switchImage = true;
+        function drawMyImage(image, x, y, tickX, tickY){
+            if(switchImage){
+                ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
+                ctx.drawImage(image, x, y, 16, 16, tickX, tickY, 16, 16);
+                switchImage = false;
+            }else{
+                ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
+                ctx.drawImage(image, x+16, y, 16, 16, tickX, tickY, 16, 16);
+                switchImage = true;
+            }
+        }
         //main animation function
         function drawStuff() {
             window.requestAnimationFrame(drawStuff);
             hero.src = "/images/character.png";
             hero.onload = function(){
                 console.log("image loaded");
-                   ctx.drawImage(hero, 0, 0, 16, 16, tickX, tickY, 16, 16);
+                drawMyImage(hero,0,0,tickX, tickY);
             };
             if (keyRight == true) {
                 tickX += 1;
                 ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
-                ctx.drawImage(hero, 0, 32, 16, 16, tickX, tickY, 16, 16);
+                var animate = setInterval(drawMyImage(hero,0,32,tickX, tickY), 1000);
             }
             if (keyBottom == true) {
                 tickY += 1;
-                ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
-                ctx.drawImage(hero, 0, 0, 16, 16, tickX, tickY, 16, 16);
+                var animate = setInterval(drawMyImage(hero,0,0,tickX, tickY), 1000);
             }
             if (keyTop == true) {
                 tickY--;
                 ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
-                ctx.drawImage(hero, 0, 16, 16, 16, tickX, tickY, 16, 16);
+                var animate = setInterval(drawMyImage(hero,0,16,tickX, tickY), 1000);
             }
             if (keyLeft == true) {
                 tickX--;
                 ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
-                ctx.drawImage(hero, 0, 48, 16, 16, tickX, tickY, 16, 16);
+                var animate = setInterval(drawMyImage(hero,0,48,tickX, tickY), 1000);
             }
         }
+
         window.requestAnimationFrame(drawStuff);
     }
 }
