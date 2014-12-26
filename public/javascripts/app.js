565ba238f055879556207412078b4813509494ec
Arnaud Huc <arnaud@MacBook-Pro-de-Arnaud.local>
2014-12-26 01:32:15 +0100

    Draw with my tiles

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,6 +3,6 @@
 var canvases = document.getElementsByTagName('canvas'),
     body = document.getElementsByTagName('body');
 for (var i = 0; i < canvases.length; i++) {
-    canvases[i].width = 1024;
-    canvases[i].height = 640;
+    canvases[i].width = 512;
+    canvases[i].height = 320;
 }
