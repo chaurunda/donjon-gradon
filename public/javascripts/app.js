f346da36c733d10ce61104c706efd30b028f159c
Arnaud Huc <arnaud@MacBook-Pro-de-Arnaud.local>
2015-01-06 03:27:07 +0100

    Add Ajax Call - Draw map on external file (start.json)

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -6,3 +6,22 @@ for (var i = 0; i < canvases.length; i++) {
     canvases[i].width = 512;
     canvases[i].height = 320;
 }
+
+function getXMLHttpRequest() {
+	var xhr = null;
+	if (window.XMLHttpRequest || window.ActiveXObject) {
+		if (window.ActiveXObject) {
+			try {
+				xhr = new ActiveXObject("Msxml2.XMLHTTP");
+			} catch(e) {
+				xhr = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+		} else {
+			xhr = new XMLHttpRequest(); 
+		}
+	} else {
+		alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
+		return null;
+	}
+	return xhr;
+}
